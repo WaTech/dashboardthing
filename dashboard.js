48a359b6bd8b20118d0ba56f83b3c371c78ae65b
justinb26 <justinb26@gmail.com>
2016-07-22 16:39:26 -0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -12,15 +12,28 @@
 // ==/UserScript==
 
 (function() {
-   'use strict';
-  
-  //run instantly and then goes after (setTimeout interval)
+
+    'use strict';
+    var api_url = 'https://dev17733.service-now.com/api/now/table/x_67288_dashboard_sites';
+
+    var sites = "";
+    $.ajax({
+      dataType: "json",
+      url: api_url,
+      async: false,
+      success: function(data, status, xhr) { sites = data; },
+      beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Basic ' + btoa('admin:DfHjaedhfWpMJ2Ueh8zM'));}, 
+    });
+
+    console.log(sites);
+    
+    //run instantly and then goes after (setTimeout interval)
   $("html, body").animate({ scrollTop: $(document).height() }, 20000);
-  
+
   setTimeout(function() {
      $('html, body').animate({scrollTop:0}, 20000);
   },20000);
-  
+
   setInterval(function(){
     // 4000 - it will take 4 secound in total from the top of the page to the bottom
     $("html, body").animate({ scrollTop: $(document).height() }, 20000);
@@ -28,17 +41,39 @@
       $('html, body').animate({scrollTop:0}, 20000);
     },20000);
   },20000);
-  
-  setTimeout(redirect, 10000);
-  
-  GM_setValue("firstSite", "https://freeboard.io/board/tmJrkl");
-  GM_setValue("newSite", "https://egov.watech.wa.gov/egovboard");
-  
+
+  setTimeout(redirect, 5000);
+
   function redirect() {
-    if (location.href == GM_getValue("firstSite")) {
-      location.href = GM_getValue("newSite");
-    } else {
-      location.href = GM_getValue("firstSite");
-    }
+    var currentUrl = location.href;
+    var newUrl = getNextSite(currentUrl);
+      console.log(currentUrl);
+      console.log(newUrl);
+    location.href = newUrl;
   }
+
+    function getNextSite(siteUrl) {
+        // Find index of siteUrl
+        var matchedIdx = null;
+
+        for (var idx in sites.result) {
+            console.log("idx " + idx);
+            console.log("sitesidx " + sites.result[idx].url);
+            if (sites.result[idx].url === siteUrl) {
+                alert("woo!");
+                matchedIdx = idx;
+            }
+        }
+
+        if (matchedIdx === null | matchedIdx === undefined) {
+            alert("oh shit");
+        } else if (matchedIdx+1 < sites.result.length) {
+            return sites.result[matchedIdx+1].url;
+        } else {
+            console.log(sites.result[0]);
+            return sites.result[0].url;
+        }
+    }
+
+
 })();
